b7602ed0dbd183f1787ad9ca00655bfea6fb2b25
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2023-03-31 20:12:48 +0300

    added new values to BikeType

--- src/main/java/com/example/model/BikeType.java
+++ src/main/java/com/example/model/BikeType.java
@@ -3,6 +3,14 @@ package com.example.model;
 import com.example.annotation.EnumInformation;
 
 public enum BikeType {
-    @EnumInformation(name = "Mountain")
-    MOUNTAIN
+    @EnumInformation(name = "Горный")
+    MOUNTAIN,
+    @EnumInformation(name = "Электровелосипед")
+    ELECTRIC,
+    @EnumInformation(name = "Дорожный")
+    ROAD,
+    @EnumInformation(name = "BMX")
+    BMX,
+    @EnumInformation(name = "Шоссейный")
+    RACING
 }
