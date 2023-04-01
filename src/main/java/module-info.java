920fd120ea439646538e86d2d432598b57ea3b37
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2023-04-01 23:02:48 +0300

    Import GSON to project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.oopcrud {
     requires javafx.controls;
     requires javafx.fxml;
+    requires gson.extras;
+    requires com.google.gson;
 
 
     opens com.example.oopcrud to javafx.fxml;
+    opens com.example.model to com.google.gson;
     exports com.example.oopcrud;
 }
