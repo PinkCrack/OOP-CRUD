d8dbd4ee4e5b3214ab81f19c97818a4ddba2537f
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2023-04-03 01:58:52 +0300

    Add Jackson to dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module com.example.oopcrud {
     requires javafx.controls;
     requires javafx.fxml;
-    requires gson.extras;
-    requires com.google.gson;
-
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
 
     opens com.example.oopcrud to javafx.fxml;
-    opens com.example.model to com.google.gson;
+    opens com.example.model to com.fasterxml.jackson.databind;
     exports com.example.oopcrud;
 }
