55aa2aa415bf9e66c8eaeab427655eb0afc09256
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2023-04-03 01:57:12 +0300

    Use Jackson json parser

--- src/main/java/com/example/serialization/JsonSerializer.java
+++ src/main/java/com/example/serialization/JsonSerializer.java
@@ -1,34 +1,29 @@
 package com.example.serialization;
 
 import com.example.model.*;
+import com.example.serialization.serializationModel.TransportSerializeModel;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 import java.io.*;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-public class JsonSerializer implements Serializer {
-    private final Type collectionType = new TypeToken<ArrayList<Transport>>() {
-    }.getType();
-    private final Gson gson;
+public class JsonSerializer extends Serializer {
 
+    private final ObjectMapper objectMapper;
+    private final ObjectWriter objectWriter;
+    private final TypeReference<ArrayList<Transport>> type = new TypeReference<ArrayList<Transport>>() {};
     public JsonSerializer()  {
-        RuntimeTypeAdapterFactory<Transport> vehicleAdapterFactory = RuntimeTypeAdapterFactory.of(Transport.class, "type")
-                .registerSubtype(Bike.class, "Bike")
-                .registerSubtype(Bus.class, "Bus")
-                .registerSubtype(GasolineCar.class, "GasolineCar")
-                .registerSubtype(ElectricCar.class, "ElectricCar");
-
-        gson = new GsonBuilder().registerTypeAdapterFactory(vehicleAdapterFactory).create();
+        objectMapper = new ObjectMapper();
+        objectWriter = objectMapper.writerFor(type);
     }
 
     @Override
     public void serialize(File file, ArrayList<Transport> listOfTransport) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            String json = gson.toJson(listOfTransport, collectionType);
+            String json = objectWriter.writeValueAsString(listOfTransport);
             bufferedWriter.write(json);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -40,7 +35,7 @@ public class JsonSerializer implements Serializer {
         ArrayList<Transport> listOfTransport;
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         String json = bufferedReader.lines().collect(Collectors.joining());
-        listOfTransport = gson.fromJson(json, collectionType);
+        listOfTransport = objectMapper.readValue(json, type);
         bufferedReader.close();
 
         return listOfTransport;
